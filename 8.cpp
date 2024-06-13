bee50a0ebd69ee0b991ee90df79fd3f64326435b
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-13 19:07:15 +0530

    modified:   .vscode/settings.json 	new file:   8.cpp

---
+++ 8.cpp
@@ -0,0 +1,33 @@
+/*
+> Date Created: June 13, 2024
+> Author: Ishaan Rastogi
+> Purpose: To show if a number is positive or negative.
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n; //cin is for user input, just like cout is for output. Eg- "( cout << n; ) will print n"
+
+    if (n>0) {
+        cout << "n is positive " << endl;
+    }
+
+    else {
+        cout << "n is negative " << endl;
+    }
+}
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
