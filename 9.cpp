ed02b7d4d986395399b250d82091fb2ec4abe72d
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-13 22:10:44 +0530

    new file:   9.cpp

---
+++ 9.cpp
@@ -0,0 +1,26 @@
+/*
+> Date Created: June 13, 2024
+> Author: Ishaan Rastogi
+> Purpose: To take 2 numbers input at same time & printing them out.
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a,b;
+    cout << "Enter the numbers: ";
+    cin >> a >> b; // After entering a, press 'enter' and then enter b. Instead of 'enter' you can also just give 'space' or 'tab'.
+    cout << "values of a & b are: " << a << " and " << b << endl;
+}
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
