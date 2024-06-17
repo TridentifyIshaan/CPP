df15c9f57affc30c51db1d0f72f08c7072c5e297
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-17 10:47:38 +0530

    new file:   26.cpp

---
+++ 26.cpp
@@ -0,0 +1,44 @@
+/*
+> Date Created: June 17, 2024
+> Author: Ishaan Rastogi
+> Purpose: To print ...
+
+Eg- For n = 4,
+
+4 3 2 1
+4 3 2
+4 3
+4
+
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: Working
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    int i = 1;
+    while ( i <= n ) {
+        int j = n; //Changing from 1 to n
+        while ( j >= i ) { //Most important line
+            cout << i <<" ";
+            j--; //Decrement
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
