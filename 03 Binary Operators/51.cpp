7344f96a0a07fbc13d45b17e7f97a6391c2146cb
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-19 18:49:17 +0530

    new file:   03 Binary Operators/51.cpp

---
+++ 03 Binary Operators/51.cpp
@@ -0,0 +1,39 @@
+/*
+> Date Created: June 19, 2024
+> Author: Ishaan Rastogi
+> Purpose: To show working of pre & post incremenet decrement operators
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int i = 7;
+    cout << (i++) << endl;
+    cout<< (++i) << endl;
+    cout << (i--) << endl;
+    cout << (--i) << endl;
+    
+}
+
+/*
+
+Output will be:
+
+8
+8
+9
+7
+
+*/
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
