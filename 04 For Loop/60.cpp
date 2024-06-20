0dcec19a87cde3eb1e7ba00487df29df5ebc71f2
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-20 11:43:08 +0530

    new file:   04 For Loop/60.cpp

---
+++ 04 For Loop/60.cpp
@@ -0,0 +1,36 @@
+/*
+> Date Created: June 20, 2024
+> Author: Ishaan Rastogi
+> Purpose: To calculate the sum of n numbers
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cout << "Enter a number: " << endl;
+    cin >> n;
+
+    int sum = 0;
+
+    for ( int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    
+    cout << sum << endl;
+
+}
+
+//Output - 0 1 2
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
