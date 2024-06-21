6b4b3ec919b3ab48505f3e0b52e1eb7b82d6c038
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-21 22:15:03 +0530

    modified:   04 For Loop/66.cpp 	new file:   04 For Loop/67.cpp

--- 04 For Loop/67.cpp
+++ 04 For Loop/67.cpp
@@ -1,7 +1,7 @@
 /*
 > Date Created: June 21, 2024
 > Author: Ishaan Rastogi
-> Purpose: Problem 3
+> Purpose: Problem 4
 > Operating System: This is only for Windows OS, it may or may not work on other OS
 > Program Status: 100% Working
 */
@@ -11,19 +11,40 @@ using namespace std;
 
 int main () {
 
-    for ( int i = 0; i <= 15; i += 2) { //increment 2 times
-        cout << i << " ";
-
-        // it ignores i afterwards 0 to 3, hence we are getting 5 instead of 6
-        if ( i & 1 ) {
-            continue;
+    for ( int i = 0; i < 5; i++) {
+        for ( int j = i; j <= 5; j++) {
+            cout << i << " " << j << endl;
         }
-        // gets ignored after increment of 0 to 3
-        i++; //increment
     }
 }
 
-//Output - 0 3 5 7 9 11 13 15
+/*
+Output -
+
+0 0
+0 1
+0 2
+0 3
+0 4
+0 5
+1 1
+1 2
+1 3
+1 4
+1 5
+2 2
+2 3
+2 4
+2 5
+3 3
+3 4
+3 5
+4 4
+4 5
+
+
+
+*/
 
 /* 
 
