1a4c7a6e10ccd18f828625aaa204063d1f6d9bc8
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-21 21:52:59 +0530

    new file:   04 For Loop/64.cpp 	new file:   04 For Loop/65.cpp

---
+++ 04 For Loop/64.cpp
@@ -0,0 +1,29 @@
+/*
+> Date Created: June 21, 2024
+> Author: Ishaan Rastogi
+> Purpose: Problem 1
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    for ( int i = 0; i <= 5; i++) {
+        cout << i << " ";
+        i++;
+    }
+}
+
+//Output - 
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
