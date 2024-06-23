d79c604b83086c05d20e1daf3bbc7026904aa113
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-23 21:04:10 +0530

    new file:   05 Scopes of variables/73.cpp

---
+++ 05 Scopes of variables/73.cpp
@@ -0,0 +1,15 @@
+/*
+> Date Created: June 23, 2024
+> Author: Ishaan Rastogi
+> Purpose: Leetcode questions
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+
+*/
+
+/*
+
+1281. Substract the product & sum of digits of an integer
+191. Number of 1 bits
+
+*/
