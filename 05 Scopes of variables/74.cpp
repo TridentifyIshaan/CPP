53301edc51e94310c8f96aab32aff8b58083dec6
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-06-24 15:19:21 +0530

    new file:   05 Scopes of variables/74.cpp

---
+++ 05 Scopes of variables/74.cpp
@@ -0,0 +1,14 @@
+/*
+> Date Created: June 24, 2024
+> Author: Ishaan Rastogi
+> Purpose: Leetcode questions
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+
+*/
+
+/*
+
+7. Reverse Integer
+
+*/
