265854876d590b77fba28c5269e50b605f2ec895
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-07-01 23:23:13 +0530

    new file:   07 Switch case & Fxns/78.cpp

---
+++ 07 Switch case & Fxns/78.cpp
@@ -0,0 +1,39 @@
+/*
+
+This note will be available only on first file of each folder present in folder C++ .
+Documentation aka basic notes on how to run the code is already kept in each file for each programme using (/*) or //.
+Wherever author needs it to be inserted, he has inserted. So that people using these files can learn easily how to code in C++ Language.
+Author has made these programmes as a reference using courses of CodingHelp - By Babbar, while himself learning C++ Language for the first time.
+
+/*
+> Date Created: July 01, 2024
+> Author: Ishaan Rastogi
+> Purpose: To show concept of switch case
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n = 2;
+    switch (2) {
+        case 1: cout << "First" << endl;
+                break;
+        case 2: cout << "Second" << endl;
+                break;
+        default: cout << "Default case" << endl;
+    }
+}
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
