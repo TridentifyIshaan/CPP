a34bb7ee7388cd02dbff25d10f84286580573a95
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-07-17 03:12:00 +0530

    new file:   07 Switch case & Fxns/95.cpp

---
+++ 07 Switch case & Fxns/95.cpp
@@ -0,0 +1,34 @@
+/*
+> Date Created: July 17, 2024
+> Author: Ishaan Rastogi
+> Purpose: Problem 1
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+
+*/
+
+#include<iostream>
+using namespace std;
+
+void update ( int a ) {
+
+    a /= 2;    
+}
+
+int main () {
+
+    int a = 10;
+    update(a);
+    cout << a << endl;
+}
+
+//Output - 10
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
