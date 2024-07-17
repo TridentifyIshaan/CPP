e714983d84bb7b370eb7d4978422eb05246ebab6
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-07-18 00:44:55 +0530

    new file:   08 Arrays, Searching, Sorting/100.cpp 	modified:   08 Arrays, Searching, Sorting/99.cpp

--- 08 Arrays, Searching, Sorting/100.cpp
+++ 08 Arrays, Searching, Sorting/100.cpp
@@ -1,24 +1,38 @@
 /*
 > Date Created: July 018, 2024
 > Author: Ishaan Rastogi
-> Purpose: To print the whole array
+> Purpose: To initial all elements of an array with 0
 > Operating System: This is only for Windows OS, it may or may not work on other OS
 > Program Status: 100% Working
 
 */
+
+// NOTE - Initialisation can onlly happen by 0, not any other number like 1, 2, ...
 
 #include <iostream>
 using namespace std;
 
 int main () {
 
-    int number[10];
+    //initialising with 0
+    int number[10] = {0};
     cout << "The array is [ ";
 
     for ( int i = 0, n = 10; i < n; i++) {
         cout << number[i] << ", ";
     }
-    cout << "]";
+    cout << "]" << endl;
+
+    //initialising with 1
+    int first[10] = {1};
+    cout << "The array is [ ";
+
+    for ( int i = 0, n = 10; i < n; i++) {
+        cout << first[i] << ", ";
+    }
+    cout << "]" << endl;
+
+    // 1 is only added at the index 0, refer to the NOTE again.
 }
 
 /* 
