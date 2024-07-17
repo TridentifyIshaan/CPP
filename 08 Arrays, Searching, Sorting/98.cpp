27b02ce61eb89684abdd5cc1de396dc201863c40
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-07-17 22:20:05 +0530

    new file:   08 Arrays, Searching, Sorting/98.cpp

---
+++ 08 Arrays, Searching, Sorting/98.cpp
@@ -0,0 +1,35 @@
+/*
+
+This note will be available only on first file of each folder present in folder C++ .
+Documentation aka basic notes on how to run the code is already kept in each file for each programme using (/*) or //.
+Wherever author needs it to be inserted, he has inserted. So that people using these files can learn easily how to code in C++ Language.
+Author has made these programmes as a reference using courses of CodingHelp - By Babbar, while himself learning C++ Language for the first time.
+
+/*
+> Date Created: July 017, 2024
+> Author: Ishaan Rastogi
+> Purpose: To show how to declare array and access any element of an array
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    //declaring array
+    int number[15] = {3, 5, 11}; // indices 0-2 has 3 values but afterwards, the rest indices left have any random garbage value.
+    //accessing an array
+    cout << "Value at 0 index is " << number[0] << endl;
+}
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
