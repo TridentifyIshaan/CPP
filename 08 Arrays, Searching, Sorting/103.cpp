af9f83849bd0fc25b9afc6a4aa11092b57357c8f
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-07-18 18:31:51 +0530

    modified:   08 Arrays, Searching, Sorting/103.cpp 	modified:   08 Arrays, Searching, Sorting/103.cpp 	08 Arrays, Searching, Sorting/104.cpp

--- 08 Arrays, Searching, Sorting/103.cpp
+++ 08 Arrays, Searching, Sorting/103.cpp
@@ -1,31 +1,54 @@
 /*
 > Date Created: July 018, 2024
 > Author: Ishaan Rastogi
-> Purpose: To show character array
+> Purpose: To find the maximum number in the array
 > Operating System: This is only for Windows OS, it may or may not work on other OS
 > Program Status: 100% Working
 
 */
 
 #include <iostream>
+#include <climits>
 using namespace std;
 
-void printArr(char arr[], int size) {
-
-    cout << "The array is [ ";
-
-    //printing the array
-    for ( int i = 0; i < size; i++) {
-        cout << arr[i]  << ", ";
+// Simple logic if you read it.
+int getMax (int num[], int n) {
+    
+    int max = INT_MIN;
+    for ( int i = 0; i < n; i++) {
+        if ( num[i] > max ) {
+            max = num[i];
+        }
     }
-    cout << "]" << endl;
+    return max;
+}
+
+// Simple logic if you read it.
+int getMin (int num[], int n) {
+    
+    int min = INT_MAX;
+    for ( int i = 0; i < n; i++) {
+        if ( num[i] > min ) {
+            min = num[i];
+        }
+    }
+    return min;
 }
 
 int main () {
 
-    char ch[5] = {'a', 'b', 'c', 'd', 'e'};
-    //cout << "The char array is " << ch << endl;
-    printArr(ch, 5);
+    int size;
+    cout << "Enter the size :" << endl;
+    cin >> size;
+
+    int num[100];
+    for ( int i = 0; i < size; i++ ) {
+        cout << "Enter the array :" << endl;
+        cin >> num[i]; 
+    }
+
+    cout << "Maximum number is " << getMax( num, size) << endl;
+    cout << "Minimum number is " << getMin( num, size) << endl;
 
 }
 
