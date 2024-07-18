8e77ed6badb31ce9741814862e32c61842c51854
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-07-18 18:33:28 +0530

    modified:   08 Arrays, Searching, Sorting/103.cpp 	new file:   08 Arrays, Searching, Sorting/104.cpp 	new file:   08 Arrays, Searching, Sorting/105.cpp

--- 08 Arrays, Searching, Sorting/103.cpp
+++ 08 Arrays, Searching, Sorting/103.cpp
@@ -1,54 +1,31 @@
 /*
 > Date Created: July 018, 2024
 > Author: Ishaan Rastogi
-> Purpose: To find the maximum number in the array
+> Purpose: To show character array
 > Operating System: This is only for Windows OS, it may or may not work on other OS
 > Program Status: 100% Working
 
 */
 
 #include <iostream>
-#include <climits>
 using namespace std;
 
-// Simple logic if you read it.
-int getMax (int num[], int n) {
-    
-    int max = INT_MIN;
-    for ( int i = 0; i < n; i++) {
-        if ( num[i] > max ) {
-            max = num[i];
-        }
-    }
-    return max;
-}
+void printArr(char arr[], int size) {
 
-// Simple logic if you read it.
-int getMin (int num[], int n) {
-    
-    int min = INT_MAX;
-    for ( int i = 0; i < n; i++) {
-        if ( num[i] > min ) {
-            min = num[i];
-        }
+    cout << "The array is [ ";
+
+    //printing the array
+    for ( int i = 0; i < size; i++) {
+        cout << arr[i]  << ", ";
     }
-    return min;
+    cout << "]" << endl;
 }
 
 int main () {
 
-    int size;
-    cout << "Enter the size :" << endl;
-    cin >> size;
-
-    int num[100];
-    for ( int i = 0; i < size; i++ ) {
-        cout << "Enter the array :" << endl;
-        cin >> num[i]; 
-    }
-
-    cout << "Maximum number is " << getMax( num, size) << endl;
-    cout << "Minimum number is " << getMin( num, size) << endl;
+    char ch[5] = {'a', 'b', 'c', 'd', 'e'};
+    //cout << "The char array is " << ch << endl;
+    printArr(ch, 5);
 
 }
 
---
+++ 08 Arrays, Searching, Sorting/105.cpp
@@ -0,0 +1,48 @@
+/*
+> Date Created: July 018, 2024
+> Author: Ishaan Rastogi
+> Purpose: To find the maximum number in the array
+> Operating System: This is only for Windows OS, it may or may not work on other OS
+> Program Status: 100% Working
+
+*/
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+// Simple logic if you read it.
+int getMin (int num[], int n) {
+    
+    int min = INT_MAX;
+    for ( int i = 0; i < n; i++) {
+        if ( num[i] > min ) {
+            min = num[i];
+        }
+    }
+    return min;
+}
+
+int main () {
+
+    int size;
+    cout << "Enter the size :" << endl;
+    cin >> size;
+
+    int num[100];
+    for ( int i = 0; i < size; i++ ) {
+        cout << "Enter the array :" << endl;
+        cin >> num[i]; 
+    }
+
+    cout << "Minimum number is " << getMin( num, size) << endl;
+}
+
+/* 
+
+For Terminal Code... Ctrl + Shift + `
+NOTE- Install Code Runner extension to run the code
+
+To Run code- Ctrl + Alt + N
+
+*/
